9251e0269077e0c85f0e7a1b2527a653395ee6d6
allan <centralkim254@gmail.com>
2025-11-04 00:26:15 +0300

    Create CAT2_Q3(iii).c

---
+++ CAT2_Q3(iii).c
@@ -0,0 +1,31 @@
+/*
+Name: Allan kimutai
+Reg: PA/106/G/28722/25
+programm that reads and display 
+*/
+void display_files() {
+FILE *input_file, *output_file;
+char ch;
+printf("--- Contents of input.txt ---\n");
+input_file = fopen("input.txt", "r");
+if (input_file == NULL) {
+printf("Error opening input file!\n");
+} else {
+while ((ch = fgetc(input_file)) != EOF) {
+putchar(ch);
+}
+fclose(input_file);
+}
+printf("\n");
+printf("--- Contents of output.txt ---\n");
+output_file = fopen("output.txt", "r");
+if (output_file == NULL) {
+printf("Error opening output file!\n");
+} else {
+while ((ch = fgetc(output_file)) != EOF) {
+putchar(ch);
+}
+fclose(output_file);
+}
+printf("\n");
+}
