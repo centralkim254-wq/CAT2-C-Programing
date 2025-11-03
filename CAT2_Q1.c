d031c74b07096cd82073f5f84976fd69cb50f4a2
allan <centralkim254@gmail.com>
2025-11-03 23:30:12 +0300

    Update CAT2_Q1.c

--- CAT2_Q1.c
+++ CAT2_Q1.c
@@ -2,20 +2,16 @@
 Name: Allan kimutai
 Reg: PA/106/G/28722/25
 programm for storing data
-
 */
 include <stdio.h>
 
 int main() {
 
 int scores[2][2] = {{65, 92}, {84, 72}};
-
 for (i = 0; i < 2; i++) {
-
 for (j = 0; j < 2; j++) {
 
 printf("%d ", scores[i][j]);
-
 printf("\n");
 
 return 0;
