308b7af6935b300d8439e83e365cc14baa534113
allan <centralkim254@gmail.com>
2025-11-04 00:08:55 +0300

    Add function to write integers to input.txt
    Implement function to write 10 integers to a file.

---
+++ CAT2_Q3(i).c
@@ -0,0 +1,21 @@
+/*
+Name: Allan kimutai
+Reg: PA/106/G/28722/25
+programm for hours_worked in a week 
+*/
+#include <stdlib.h>
+void write_to_file() {
+FILE *file_ptr;
+int num;
+file_ptr = fopen("input.txt", "w");
+if (file_ptr == NULL) {
+printf("Error opening file!\n");
+return;
+}
+printf("Enter 10 integers:\n");
+for (int i = 0; i < 10; i++) {
+scanf("%d", &num);
+fprintf(file_ptr, "%d ", num);
+}
+fclose(file_ptr);
+}
